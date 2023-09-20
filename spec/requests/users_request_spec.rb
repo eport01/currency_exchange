d4a5406fa565d68102e1f6593e6c98719fe1994f
Emily Port <emilylport@gmail.com>
2023-09-20 10:04:05 -0600

    add login sad path test for user that doesnt exist

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'users requests' do
     @user.update(api_key: SecureRandom.hex)
   end
   describe 'login' do 
-    it 'a user can login' do 
+    it 'a user can login', :vcr do 
 
       login_credentials = {
         email: @user.email,
@@ -16,6 +16,17 @@ RSpec.describe 'users requests' do
 
       expect(response).to have_http_status(:ok)
 
+    end
+  end
+
+  describe 'sad path login', :vcr do 
+    it 'if credentials are bad, a user cant login' do 
+      get "/login", headers: {'CONTENT_TYPE' => 'application/json'}, params: {email: "email@email.com", password: "456"}
+
+      expect(response).to have_http_status(404)
+      error_result = JSON.parse(response.body, symbolize_names: true)
+
+      expect(error_result[:error]).to eq("Unable to login")
     end
   end
 end
